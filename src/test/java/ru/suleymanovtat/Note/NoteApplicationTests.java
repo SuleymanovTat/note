7ef4a80a5d5c708a83b82ad4353dda22928da5d5
suleymanovtat <suleymanovtat@gmail.com>
2020-11-24 22:25:24 +0300

    init project

---
+++ src/test/java/ru/suleymanovtat/Note/NoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.suleymanovtat.Note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
